89c4dd3055a2937843922a7e41dfb93c6bfa5067
Bree <peyton.sandridge@gmail.com>
2024-07-10 22:34:38 -0400

    creating square tiles

---
+++ src/lib/BingoBoard.svelte
@@ -0,0 +1,69 @@
+<script>
+import BingoCard from "$lib/BingoCard.svelte";
+
+// let outer = document.getElementById('outer'),
+//     wrapper = document.getElementById('wrap'),
+//     maxWidth  = outer.clientWidth,
+//     maxHeight = outer.clientHeight;
+//
+// window.addEventListener("resize", resize);
+// resize();
+
+let prompts = [
+    {name: "Prog"},
+    {name: "Writing"},
+    {name: "Writing"},
+    {name: "Writing"},
+    {name: "Writing"},
+    {name: "Writing"},
+    {name: "Writing"},
+    {name: "Writing"},
+    {name: "Writing"},
+    {name: "Writing"},
+    {name: "Writing"}
+]
+
+// function resize() {
+//     let scale;
+//     let width = window.innerWidth;
+//     let height = window.innerHeight;
+//
+//     let isMax = width >= maxWidth && height >= maxHeight;
+//
+//     scale = Math.min(width/maxWidth, height/maxHeight);
+//     outer.style.transform = isMax?'':'scale(' + scale + ')';
+//     wrapper.style.width = isMax?'':maxWidth * scale;
+//     wrapper.style.height = isMax?'':maxHeight * scale;
+// }
+
+</script>
+
+
+
+<div class="board">
+    {#each prompts as {name}}
+        <BingoCard name={name} />
+    {/each}
+
+</div>
+
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
+    .board {
+        /* TODO: Make it so entire board fits on screen */
+        display: grid;
+        height: min(calc(50vh - 5px), calc(50vw - 5px));
+        aspect-ratio: 1 / 1;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
+
+    .board {
+        counter-reset: test;
+    }
+
+</style>
---
+++ src/lib/BingoCard.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let name;
+
+
+</script>
+
+
+<div class="card">
+    <div class="card-body">
+        <p>{name}</p>
+    </div>
+</div>
+
+<style>
+    .card {
+        /* bg to show i'm squared or not ? */
+        background-image: linear-gradient( 45deg, transparent 50%, rgba(0, 0, 0, 0.5) 50%);
+        aspect-ratio: 1/1;
+
+        margin: 0 5px 5px 0;  /*instead playing around with grid gap gap */
+        background-color: red;
+    }
+
+    p {
+        word-wrap: break-word;
+    }
+</style>
